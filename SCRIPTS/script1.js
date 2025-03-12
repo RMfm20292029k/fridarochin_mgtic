5aadab777bdbbc214cce4f1ff7316ee1789e5d1d
Frida Rochin <fridarochin@Fridas-MacBook-Air.local>
2025-03-11 19:29:09 -0700

    avance

---
+++ SCRIPTS/script1.js
@@ -0,0 +1,32 @@
+//keyword variableName = value;
+// var, let, const
+
+//String 
+var firstname = "Frida";
+var lastname = "Rochin";
+
+//Concatenation
+
+firstname = "Frida";
+console.log("name: "+firstname + " " + lastname);
+
+//numerical
+
+let myage=99;
+let myheight=100;
+
+console.log(myage,myheight);
+
+//boolean (true/false)
+let isstudent = true;
+
+//math operations + - / *
+let num1=10;
+let num2=12;
+
+document.write("<p>el resultado es = " + (num1+num2) + "</p>");
+document.write("<p>el resultado es = " + (num1-num2)+ "</p>");
+document.write("<p>el resultado es = " + (num1*num2)) + "</p>";
+document.write("<p>el resultado es = " + (num1/num2).toFixed(3)+ "</p>");
+
+alert("esto es un mensaje de alerta");
